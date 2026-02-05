842e63fd26bf246ae8b54efd28940088a065b99e
cs3n21 <226476376+hw-csong@users.noreply.github.com>
2026-02-05 20:13:09 +0800

    test case

--- tests/accuracy/functions/test_dllm_attention_up.py
+++ tests/accuracy/functions/test_dllm_attention_up.py
@@ -53,12 +53,12 @@ def generate_test_data(
     [
         pytest.param(
             *generate_test_data(
-                q_head_num=8,
-                kv_head_num=2,
+                q_head_num=5,
+                kv_head_num=1,
                 head_dim=128,
-                max_seq_length=8192,
+                max_seq_length=32768,
                 block_size=8,
-                sample_min=128,
+                sample_min=1024,
                 sample_max=1024,
             )
         ),
